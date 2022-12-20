eacf2b404dc6f294a58d8a66ae476d0c0346360a
simran1002 <simran2131056@akgec.ac.in>
2022-12-20 19:47:07 +0530

    updated files

--- server.js
+++ server.js
@@ -6,15 +6,25 @@ const expressLayout = require('express-ejs-layouts')
 const PORT = process.env.PORT || 3000
 
 
-app.get('/', (req,res) => {
-    res.render('home')
-})
-
 
 //set Template engine
 app.use(expressLayout)
 app.set('views', path.join(__dirname, './resources/views'))
 app.set('view engine', 'ejs')
+
+
+//Assests
+app.use(express.static('public'))
+app.get('/', (req,res) => {
+    res.render('landingpage')
+})
+
+
+app.get('/contact',(req,res) =>{
+    res.render('home')
+})
+
+
 
 app.listen(PORT , () => {
     console.log(`Listening on port ${PORT} `)
